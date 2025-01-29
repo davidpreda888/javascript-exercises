adcd3a8f85290dfae13f5d5712921d7064692db9
DavidP <171104270+davidpreda888@users.noreply.github.com>
2025-01-28 23:44:10 -0500

    Finish Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let result = 0;
+
+    if(int1 < 0 || int2 < 0 || typeof int1 !== 'number' || typeof int2 !== 'number' || Math.floor(int1) !== int1 || Math.floor(int2) !== int2){
+        return "ERROR";
+    }
+
+    for (let i= Math.min(int1, int2); i <= Math.max(int1, int2); i++){
+        result = result + i;
+    }
+    return result;
 
 };
 
