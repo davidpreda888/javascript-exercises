e957f7333061b0bd2aa61f1cffdc7bf627dd9794
DavidP <171104270+davidpreda888@users.noreply.github.com>
2025-01-29 00:03:31 -0500

    Finish Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  return Math.round((fahren - 32) * (50/9)) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(celsius * (90/5) + 320) / 10
 };
 
 // Do not edit below this line
